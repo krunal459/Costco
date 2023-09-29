9a65237257096f78251d7777f3c1ef5d4ce0117e
krunal459 <142106928+krunal459@users.noreply.github.com>
2023-09-29 07:53:37 -0500

    Update DealsTest.java
    changed

--- costco/src/test/java/test/com/DealsTest.java
+++ costco/src/test/java/test/com/DealsTest.java
@@ -1,10 +1,12 @@
 package test.com;
 
+
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Action;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.AfterClass;
@@ -12,7 +14,6 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
 public class DealsTest {
 
     private WebDriver driver;
